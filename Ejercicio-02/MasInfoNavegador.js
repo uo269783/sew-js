2a62d1571590cf995d25609419486ca9b515fa8b
unknown <uo269783@uniovi.es>
2022-11-22 21:20:20 +0100

    a

--- Ejercicio-02/MasInfoNavegador.js
+++ Ejercicio-02/MasInfoNavegador.js
@@ -1,5 +1,5 @@
-document.write("Version: "+infoNavegador.version+"\n");
-document.write("Plataforma: "+infoNavegador.plataforma+"\n");
-document.write("Vendedor: "+infoNavegador.vendedor+"\n");
-document.write("Agente: "+infoNavegador.agente+"\n");
-document.write("¿Java?: "+infoNavegador.javaActivo+"\n");
+document.write("Version: " + infoNavegador.version + "\n");
+document.write("Plataforma: " + infoNavegador.plataforma + "\n");
+document.write("Vendedor: " + infoNavegador.vendedor + "\n");
+document.write("Agente: " + infoNavegador.agente + "\n");
+document.write("¿Java?: " + infoNavegador.javaActivo + "\n");
