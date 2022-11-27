ffd74e91ca9db0caf601bb3520f0a055e6d539f4
unknown <uo269783@uniovi.es>
2022-11-27 02:40:29 +0100

    aaa

--- Ejercicio-07/Ejercicio7.js
+++ Ejercicio-07/Ejercicio7.js
@@ -1,6 +1,7 @@
 var parrafoMostrado = true;
 var titulo1Mostrado = true;
 var nParrafos = 1;
+var nElementosTabla = 1;
 
 $('document').ready(function () {
     $('input[type=button]:nth-child(3)').click(function () {
@@ -38,10 +39,37 @@ $('document').ready(function () {
         $('p:last-of-type').remove();
     });
 
-    $('input[type=button]:nth.child(8)').click(function () {
-        $("*", "document").each()
+    $('input[type=button]:nth-child(8)').click(function () {
+        var padre;
+        var tipo;
+        $('h2').val("Árbol DOM recorrido:");
+        $("*", document.body).each(function () {
+            padre = $(this).parent().get(0).tagName;
+            tipo = $(this).get(0).tagName;
+
+
+            $('h2').after("<p>Elemento: " + tipo + ", etiqueta del padre: " + padre + ". </p>");
+        });
     });
 
+    $('input[type=button]:nth-child(9)').click(function () {
+        var nFilas = $('tr').length;
 
+        var nColumnas = $('*', 'tr:first-child').length;
 
-})
+        $('p:last-of-type').after("<p>Filas: " + nFilas + ", columnas: " + nColumnas + ", suma: " + (nFilas + nColumnas) + ", número de elementos: " + (nFilas * nColumnas));
+
+    });
+
+    $('input[type=button]:nth-child(10)').click(function () {
+        var nColumnas = $('*', 'tr:first-child').length;
+        var fila = "<tr> <th>Encabezado" + (nElementosTabla++) + "</th>";
+        for (var i = 0; i < nColumnas - 1; i++) {
+            fila += "<td>Elemento nº " + (nElementosTabla++) + "</td>";
+        }
+
+        fila += "</tr>";
+        $('table tr:last-child').after(fila);
+    });
+
+});
