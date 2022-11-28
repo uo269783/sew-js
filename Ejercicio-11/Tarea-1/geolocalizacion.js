3d013df6a5b8d30bb846b3df7f7187ed2bc2e0df
unknown <uo269783@uniovi.es>
2022-11-28 01:31:20 +0100

    aaaa

--- Ejercicio-11/Tarea-1/geolocalizacion.js
+++ Ejercicio-11/Tarea-1/geolocalizacion.js
@@ -1,5 +1,21 @@
 class Geolocalizacion {
-    constructor() {
 
+
+    constructor() {
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
     }
+
+    getPosicion(val) {
+        this.longitud = val.coords.longitude;
+        this.latitud = val.coords.latitude;
+    }
+
+    escribirDatos() {
+        $('p:last-child').after("<p>Longitud: " + this.longitud + ", latitud: " + this.latitud + "</p>");
+    }
+
+
 }
+
+var g = new Geolocalizacion();
+g.escribirDatos();
