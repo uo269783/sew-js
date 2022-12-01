62bfcd7c2e3168f79b5fa6702347f757725e7577
uo269783 <uo269783@uniovi.es>
2022-12-01 12:51:59 +0100

    aaaa

--- Ejercicio-12/Ejercicio12.js
+++ Ejercicio-12/Ejercicio12.js
@@ -1,6 +1,48 @@
-class LectorArchivos {
+class Lector {
 
     constructor() {
-        
     }
+
+    escribirTexto(reader) {
+        //$('section:first').text(reader.result);
+        var parrafos = reader.result.split("\n");
+        $('section:last').text("");
+        $('section:last').append("<h2>Contenido</h2>");
+        for (var p in parrafos)
+            if (parrafos[p].trim() != "")
+                $('section:first').append("<p>" + parrafos[p] + "</p>");
+    }
+
+    leerArchivo(files) {
+        var archivo = files[0];
+        var reader = new FileReader();
+        var context = this;
+        reader.onload = function () { context.escribirTexto(this) };
+
+        console.log(archivo.type);
+        switch (archivo.type) {
+            case "text/plain":
+            case "text/xml":
+            case "text/json":
+                reader.readAsText(archivo);
+                break;
+        }
+
+        this.escribirDatos(archivo);
+    }
+
+    escribirDatos(archivo) {
+        $('section:last').text("");
+        $('section:last').append("<h2>Datos</h2>");
+
+        var nombre = "<p>Nombre del archivo: " + archivo.name + "</p>";
+        var tamaño = "<p>Tamaño del archivo: " + archivo.size + "</p>";
+        var tipo = "<p>Tipo del archivo: " + archivo.type + "</p>";
+
+        $('section:last').append(nombre + tamaño + tipo);
+    }
+
+
 }
+
+var lector = new Lector();
